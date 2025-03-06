87df4c5b8ae01069710cbb576e398a8b112ac1cc
viethoangg0902 <viethoangg0902@gmail.com>
2025-03-06 11:50:21 +0700

    regex email

--- src/Screen/UsersScreen/modalUpdate.js
+++ src/Screen/UsersScreen/modalUpdate.js
@@ -9,7 +9,7 @@ const UserEditModal = ({ visible, onUpdate, onCancel }) => {
     form.resetFields();
     onCancel();
   };
-  
+
   const handleOk = async () => {
     try {
       const values = await form.validateFields();
@@ -26,7 +26,7 @@ const UserEditModal = ({ visible, onUpdate, onCancel }) => {
         <Form.Item name="userName" label="Tên đăng nhập" rules={[{ required: true, message: "Vui lòng nhập tên đăng nhập!" }]}>
           <Input />
         </Form.Item>
-        <Form.Item name="email" label="Email" rules={[{ required: true, message: "Vui lòng nhập Email!" }]}>
+        <Form.Item name="email" label="Email" rules={[{ required: true, message: "Vui lòng nhập Email!" }, { type: "email", message: 'Email không đúng định dạng' }]}>
           <Input />
         </Form.Item>
         <Form.Item name="password" label="Password" rules={[{ required: true, message: "Vui lòng nhập password!" }]}>
