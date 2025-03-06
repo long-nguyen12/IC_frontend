0f015cf86e3cac66a538850814e59d07d9bde2db
Nguyen Long <longnguyenhoang.ict@gmail.com>
2025-03-06 15:40:46 +0700

    fix babel

--- src/Screen/ListForderScreen/ListFoder.js
+++ src/Screen/ListForderScreen/ListFoder.js
@@ -1,16 +1,16 @@
-import { LoadingOutlined, PlusOutlined, LockOutlined } from '@ant-design/icons';
-import { Flex, message, Upload, Avatar, Space, Select } from 'antd';
+import { LoadingOutlined, PlusOutlined, LockOutlined } from "@ant-design/icons";
+import { Flex, message, Upload, Avatar, Space, Select } from "antd";
 import React, { useState, useEffect } from "react";
 import { API } from "../../constants/API";
 import request from "../../service/request";
-import { Input, Image, Button, Modal } from 'antd';
-import { useSelector } from 'react-redux';
-import { Col, Divider, Row, Spin, Alert } from 'antd';
-import DetectionImage from '../ListImgScreen/DetectionImage'
-import { formatString } from '../../constants/formatString'
+import { Input, Image, Button, Modal } from "antd";
+import { useSelector } from "react-redux";
+import { Col, Divider, Row, Spin, Alert } from "antd";
+import DetectionImage from "../ListImgScreen/DetectionImage";
+import { formatString } from "../../constants/formatString";
 import { useLocation, useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import AppBreadcrumb from '../../component/bread'
+import AppBreadcrumb from "../../component/bread";
 const ListFoderScreen = () => {
   const [foderName, setFoderName] = useState(`all`);
   const [foder, setFoder] = useState({});
@@ -20,19 +20,17 @@ const ListFoderScreen = () => {
 
   const navigate = useNavigate();
   let location = useLocation();
-  console.log("location", location)
+  
   const handleGetFolder = async () => {
     await request
       .get(API.FOLDERS + `/${location.state.fodername}`)
       .then((res) => {
         if (res?.data) {
-          setFoder(res.data.data)
+          setFoder(res.data.data);
         }
       })
       .catch((err) => console.log(err));
   };
-
-  console.log("foder--------------------",foder)
 
   function removeFirstSegment(pathStr) {
     const parts = pathStr.split(/[/\\]/);
@@ -46,44 +44,60 @@ const ListFoderScreen = () => {
     return parts[1];
   }
 
-
   useEffect(() => {
-    handleGetFolder()
+    handleGetFolder();
   }, [location.state.fodername]);
 
-
   const SelectFoder = async (path) => {
-    console.log("path---------", API.FOLDERS + path)
+    console.log("path---------", API.FOLDERS + path);
     await request
-      .get(API.FOLDERS + '/' + path)
+      .get(API.FOLDERS + "/" + path)
       .then((res) => {
         if (res?.data) {
-          setFoder(res.data.data)
+          setFoder(res.data.data);
         }
       })
       .catch((err) => console.log(err));
+  };
+
+  const downloadJson = (foldername) => {
+    console.log(foldername);
   }
-
-
-
-
 
   return (
     <div>
-      {foder ?
+      {foder ? (
         <div>
-
           <AppBreadcrumb />
-          <div>{foder.name}</div>
+          <Row justify="space-between" align="center">
+            <div>{foder.name}</div>
+            {location?.state?.fodername !== "all" && (
+              <Row>
+                <Button type="primary" style={{ marginRight: 10 }} onClick={() => downloadJson(location?.state?.fodername)}>
+                  Export JSON
+                </Button>
+                <Button color="danger" variant="solid">
+                  Xoá
+                </Button>
+              </Row>
+            )}
+          </Row>
 
           <Row gutter={16}>
-            {foder.children?.map(item => (
+            {foder.children?.map((item) => (
               <Col className="gutter-row" span={3} key={item.name}>
-                <div className='poiter' onClick={() => navigate(`/foders/${item.name}`, { state: { fodername: `${item.path}` } })}>
-                  <div className='foder-box' >
-
+                <div
+                  className="poiter"
+                  onClick={() =>
+                    navigate(`/foders/${item.name}`, {
+                      state: { fodername: `${item.path}` },
+                    })
+                  }
+                >
+                  <div className="foder-box"></div>
+                  <div className="name-foder">
+                    <span>{item.name}</span>
                   </div>
-                  <div className='name-foder'><span>{item.name}</span></div>
                 </div>
               </Col>
             ))}
@@ -107,24 +121,30 @@ const ListFoderScreen = () => {
                       src={formatString(
                         API.API_HOST + API.VIEW_IMAGE,
                         returnFoderSegment(item.path),
-                        removeFirstSegment(item.path.replace(/^uploads[\/\\]?/, ""))
+                        removeFirstSegment(
+                          item.path.replace(/^uploads[\/\\]?/, "")
+                        )
                       )}
                       preview={false}
                       onClick={() =>
-                        navigate(`/image/${ returnFoderSegment(item.path)}`, { state: {index: index } })
+                        navigate(`/image/${returnFoderSegment(item.path)}`, {
+                          state: { index: index },
+                        })
                       }
                       style={{ width: "100%" }}
                     />
                   </div>
                   <div className="name-foder">
-                    <span>{item.name?.substring(item.name.length - 10)} {index}</span>
+                    <span>
+                      {item.name?.substring(item.name.length - 10)} {index}
+                    </span>
                   </div>
                 </div>
               </Col>
             ))}
           </Row>
         </div>
-        :
+      ) : (
         <div>
           <Spin tip="Loading...">
             <Alert
@@ -134,9 +154,8 @@ const ListFoderScreen = () => {
             />
           </Spin>
         </div>
-      }
+      )}
     </div>
-  )
-
-}
+  );
+};
 export default ListFoderScreen;
